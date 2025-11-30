597466f31a716811cba228a60f6c5847a7b8e96a
Пшеничный Иван <pshenichnyy.i@list.ru>
2025-11-30 20:03:31 +0300

    add deploy configs

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,40 @@
+require("dotenv").config({ path: "../.env.deploy" });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } =
+  process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "api",
+      script: "./dist/app.js",
+      env: { NODE_ENV: "production" },
+      instances: 1,
+      watch: false,
+      autorestart: true,
+      max_memory_restart: "300M",
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      "pre-deploy-local":
+        "scp .env $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/backend/.env",
+
+      "post-deploy": [
+        "cd backend",
+
+        "npm install",
+
+        "npm run build",
+
+        "pm2 startOrReload ecosystem.config.js --env production --update-env",
+      ].join(" && "),
+    },
+  },
+};
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: '../.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF,
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      ssh_options: 'StrictHostKeyChecking=no',
+      
+      'post-deploy': [
+        'cd frontend',
+
+        'npm install',
+
+        'NODE_OPTIONS=--openssl-legacy-provider npm run build',
+      ].join(' && '),
+    },
+  },
+};
