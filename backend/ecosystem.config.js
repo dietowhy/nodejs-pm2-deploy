58f14146152cc09d7f240d7b14724ca1b990a1c3
Пшеничный Иван <pshenichnyy.i@list.ru>
2025-11-30 22:17:12 +0300

    fix4

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,6 @@
 require("dotenv").config({ path: "../.env.deploy" });
 
-const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } =
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO, DEPLOY_SSH_KEY} =
   process.env;
 
 module.exports = {
@@ -20,11 +20,13 @@ module.exports = {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
+      key: DEPLOY_SSH_KEY,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
+      ssh_options: 'StrictHostKeyChecking=no',
       "pre-deploy-local":
-        "bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}",
+        `scp -i ${DEPLOY_SSH_KEY} ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend/`,
 
       "post-deploy": [
         "cd backend",
