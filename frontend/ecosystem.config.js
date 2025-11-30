693372c7ce9fc719bb36def83a05e7a08623686a
Пшеничный Иван <pshenichnyy.i@list.ru>
2025-11-30 22:49:35 +0300

    fix4

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,26 +1,26 @@
-require('dotenv').config({ path: '../.env.deploy' });
+require("dotenv").config({ path: "../.env.deploy" });
 
-const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF,
-} = process.env;
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF } =
+  process.env;
 
 module.exports = {
   deploy: {
     production: {
       user: DEPLOY_USER,
+      key: DEPLOY_SSH_KEY,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      ssh_options: 'StrictHostKeyChecking=no',
-      
-      'post-deploy': [
-        'cd frontend',
+      ssh_options: "StrictHostKeyChecking=no",
 
-        'npm install',
+      "post-deploy": [
+        "cd frontend",
 
-        'NODE_OPTIONS=--openssl-legacy-provider npm run build',
-      ].join(' && '),
+        "npm install",
+
+        "NODE_OPTIONS=--openssl-legacy-provider npm run build",
+      ].join(" && "),
     },
   },
 };
